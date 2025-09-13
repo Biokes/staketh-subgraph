6c18dec361ba7fdc011d70d1a8daa6af52311a79
Biokes <106444765+Biokes@users.noreply.github.com>
2025-09-13 14:58:24 +0100

    update schema

--- src/staking-contract.ts
+++ src/staking-contract.ts
@@ -1,211 +1,87 @@
+import { Address, Bytes, ethereum } from "@graphprotocol/graph-ts";
 import {
   EmergencyWithdrawn as EmergencyWithdrawnEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  Paused as PausedEvent,
   RewardRateUpdated as RewardRateUpdatedEvent,
   RewardsClaimed as RewardsClaimedEvent,
   Staked as StakedEvent,
   StakingInitialized as StakingInitializedEvent,
-  StakingPaused as StakingPausedEvent,
-  StakingUnpaused as StakingUnpausedEvent,
-  TokenRecovered as TokenRecoveredEvent,
-  Unpaused as UnpausedEvent,
-  Withdrawn as WithdrawnEvent
-} from "../generated/StakingContract/StakingContract"
+  Withdrawn as WithdrawnEvent,
+} from "../generated/StakingContract/StakingContract";
 import {
   EmergencyWithdrawn,
-  OwnershipTransferred,
-  Paused,
-  RewardRateUpdated,
   RewardsClaimed,
   Staked,
-  StakingInitialized,
-  StakingPaused,
-  StakingUnpaused,
-  TokenRecovered,
-  Unpaused,
-  Withdrawn
-} from "../generated/schema"
+  Withdrawn,
+  Transaction,
+  User,
+} from "../generated/schema";
+
+export function handleCreateUser(userAddress: Address): User {
+  let user = User.load(userAddress as Bytes);
+  if (user == null) {
+    user = new User(userAddress as Bytes);
+    user.save();
+  }
+  return user as User;
+}
+
+export function handleCreateTransaction(event: ethereum.Event): Bytes {
+  let entity = new Transaction(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.timestamp = event.block.timestamp;
+  entity.save();
+  return entity.id;
+}
+
 
 export function handleEmergencyWithdrawn(event: EmergencyWithdrawnEvent): void {
   let entity = new EmergencyWithdrawn(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.amount = event.params.amount
-  entity.penalty = event.params.penalty
-  entity.timestamp = event.params.timestamp
-  entity.newTotalStaked = event.params.newTotalStaked
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePaused(event: PausedEvent): void {
-  let entity = new Paused(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRewardRateUpdated(event: RewardRateUpdatedEvent): void {
-  let entity = new RewardRateUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.oldRate = event.params.oldRate
-  entity.newRate = event.params.newRate
-  entity.timestamp = event.params.timestamp
-  entity.totalStaked = event.params.totalStaked
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  );
+  entity.user = handleCreateUser(event.params.user).id;
+  entity.amount = event.params.amount;
+  entity.penalty = event.params.penalty;
+  entity.newTotalStaked = event.params.newTotalStaked;
+  entity.transactionData = handleCreateTransaction(event);
+  entity.save();
 }
 
 export function handleRewardsClaimed(event: RewardsClaimedEvent): void {
   let entity = new RewardsClaimed(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.amount = event.params.amount
-  entity.timestamp = event.params.timestamp
-  entity.newPendingRewards = event.params.newPendingRewards
-  entity.totalStaked = event.params.totalStaked
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  );
+  entity.user = handleCreateUser(event.params.user).id;
+  entity.amount = event.params.amount;
+  entity.newPendingRewards = event.params.newPendingRewards;
+  entity.totalStaked = event.params.totalStaked;
+  entity.transactionData = handleCreateTransaction(event);
+  entity.save();
 }
 
 export function handleStaked(event: StakedEvent): void {
   let entity = new Staked(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.amount = event.params.amount
-  entity.timestamp = event.params.timestamp
-  entity.newTotalStaked = event.params.newTotalStaked
-  entity.currentRewardRate = event.params.currentRewardRate
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleStakingInitialized(event: StakingInitializedEvent): void {
-  let entity = new StakingInitialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.stakingToken = event.params.stakingToken
-  entity.initialRewardRate = event.params.initialRewardRate
-  entity.timestamp = event.params.timestamp
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleStakingPaused(event: StakingPausedEvent): void {
-  let entity = new StakingPaused(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.timestamp = event.params.timestamp
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleStakingUnpaused(event: StakingUnpausedEvent): void {
-  let entity = new StakingUnpaused(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.timestamp = event.params.timestamp
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTokenRecovered(event: TokenRecoveredEvent): void {
-  let entity = new TokenRecovered(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.token = event.params.token
-  entity.amount = event.params.amount
-  entity.timestamp = event.params.timestamp
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUnpaused(event: UnpausedEvent): void {
-  let entity = new Unpaused(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  );
+  entity.user = handleCreateUser(event.params.user).id;
+  entity.amount = event.params.amount;
+  entity.newTotalStaked = event.params.newTotalStaked;
+  entity.currentRewardRate = event.params.currentRewardRate;
+  entity.transactionData = handleCreateTransaction(event);
+  entity.save();
 }
 
 export function handleWithdrawn(event: WithdrawnEvent): void {
   let entity = new Withdrawn(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.amount = event.params.amount
-  entity.timestamp = event.params.timestamp
-  entity.newTotalStaked = event.params.newTotalStaked
-  entity.currentRewardRate = event.params.currentRewardRate
-  entity.rewardsAccrued = event.params.rewardsAccrued
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  );
+  entity.user = handleCreateUser(event.params.user).id;
+  entity.amount = event.params.amount;
+  entity.newTotalStaked = event.params.newTotalStaked;
+  entity.currentRewardRate = event.params.currentRewardRate;
+  entity.rewardsAccrued = event.params.rewardsAccrued;
+  entity.transactionData = handleCreateTransaction(event);
+  entity.save();
 }
